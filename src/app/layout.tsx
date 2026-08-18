2aef2b47dae6926c2ebf6f4593f9aed431459d79
davidkim <davidkim@ascentnet.co.jp>
2026-08-18 15:46:56 +0900

    feat: Google Tag Manager(GTM-P7QHGQVS) 코드 전체 페이지에 적용
    - head 최상단에 GTM 컨테이너 스니펫 추가
    - body 시작 직후 noscript iframe 추가
    - 결제완료 페이지의 purchase dataLayer 코드는 별도 프로젝트(watcher/shindan 앱)에 적용 필요

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,13 @@ export default function RootLayout({
   return (
     <html lang="ja" className="h-full antialiased">
       <head>
+        <Script id="gtm-script" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P7QHGQVS');`}
+        </Script>
         <link rel="preconnect" href="https://cdn.jsdelivr.net" />
         <link
           rel="stylesheet"
@@ -57,6 +64,14 @@ export default function RootLayout({
         />
       </head>
       <body className="min-h-full flex flex-col">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P7QHGQVS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-H7YRT79KK0"
           strategy="afterInteractive"
